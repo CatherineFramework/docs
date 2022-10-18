3544e4dc357e298373e1c38682a44ce3d5ecdfa7
Hifumi1337 <hifumi1337@protonmail.com>
2022-10-18 13:15:55 -0500

    Removed env variables

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = require("nextra")({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.js",
     darkmode: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    basePath: '/catherine-docs',
+    assetPrefix: '/catherine-docs/',
     images: { unoptimized: true }
 });
 
