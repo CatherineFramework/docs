1d1419b6df971301a62acaf3a66b81ec0a17a131
Hifumi1337 <hifumi1337@protonmail.com>
2022-10-18 13:23:23 -0500

    Config changes

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = require("nextra")({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.js",
     darkmode: true,
-    basePath: '/catherine-docs',
-    assetPrefix: '/catherine-docs/',
     images: { unoptimized: true }
 });
 
