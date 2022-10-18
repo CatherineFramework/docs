29e5ba4123dc89f513da507a2f5c81dc195a4a84
Hifumi1337 <hifumi1337@protonmail.com>
2022-10-18 03:56:35 -0500

    Minor config updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require("nextra")({
+const nextConfig = require("nextra")({
     theme: "nextra-theme-docs",
-    themeConfig: "./theme.config.js"
+    themeConfig: "./theme.config.js",
+    images: { unoptimized: true }
 });
 
-module.exports = withNextra();
+module.exports = nextConfig();
