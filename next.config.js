4e7691dabdffa3b6e9428be128e12032a23e8eef
Hifumi1337 <hifumi1337@protonmail.com>
2022-10-18 04:03:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const nextConfig = require("nextra")({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.js",
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    basePath: '/catherine-docs',
+    assetPrefix: '/catherine-docs/'
 });
 
 module.exports = nextConfig();
