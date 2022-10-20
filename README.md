7f68433ee55c89c030f25bf244773e8e098212c6
Hifumi1337 <hifumi1337@protonmail.com>
2022-10-19 19:47:34 -0500

    v0.1.10
    - Finally finished with ~90% of the docs!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Catherine Framework Documentation
+This repository contains the documentation for the [Catherine framework](https://github.com/Hifumi1337/catherine).
+
+[![Documentation Status](https://api.netlify.com/api/v1/badges/2a468b5b-70fb-4fb2-86d5-8a19364da558/deploy-status)](https://app.netlify.com/sites/darling-rolypoly-d85199/deploys)
--- theme.config.js
+++ theme.config.js
@@ -1,14 +1,10 @@
 export default {
     github: "https://github.com/Hifumi1337/catherine-docs",
-    docsRepositoryBase:
-        "https://github.com/Hifumi1337/catherine-docs/tree/main",
+    docsRepositoryBase: "https://github.com/Hifumi1337/catherine-docs/tree/main",
     titleSuffix: " | Catherine Framework",
     logo: (
         <>
-            <span
-                className="mr-2 font-extrabold hidden md:inline"
-                style={{ fontSize: "25px" }}
-            >
+            <span className="mr-2 font-extrabold hidden md:inline" style={{ fontSize: "25px" }}>
                 Catherine Framework
             </span>
         </>
@@ -17,25 +13,15 @@ export default {
         <>
             <meta name="msapplication-TileColor" content="#ffffff" />
             <meta name="theme-color" content="#ffffff" />
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta httpEquiv="Content-Language" content="en" />
-            <meta
-                name="description"
-                content="Catherine framework documentation"
-            />
-            <meta
-                name="og:description"
-                content="Catherine framework documentation"
-            />
-            <meta
-                name="apple-mobile-web-app-title"
-                content="Catherine framework documentation"
-            />
+            <meta name="description" content="Catherine framework documentation" />
+            <meta name="keywords" content="blue team, defense framework, rust cybersecurity framework, rust framework, rust blue team framework, hifumi1337, catherine framework documentation, catherine framework" />
+            <meta name="og:description" content="Catherine framework documentation" />
+            <meta name="apple-mobile-web-app-title" content="Catherine framework documentation" />
         </>
     ),
+
     search: true,
     prevLinks: true,
     nextLinks: true,
